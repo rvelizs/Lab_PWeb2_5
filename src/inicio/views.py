760e02d26a712ba64bf6a04674f726e82e687fdc
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-10 01:20:14 -0500

    Django 02 / Nueva vista another

--- src/inicio/views.py
+++ src/inicio/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwards):
     return HttpResponse('<h1>Hola Mundo desde Django</h1>')
+
+def anotherView(request):
+    return HttpResponse('<h1>Solo otra página</h1>')
