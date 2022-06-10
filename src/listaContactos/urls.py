2cf49b5a2116f1996c9cef01b7f8dcc097cc4620
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-10 00:47:07 -0500

    Django 02 / Hola Mundo desde Django

--- src/listaContactos/urls.py
+++ src/listaContactos/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inicio.views import myHomeView
 
 urlpatterns = [
+    path('', myHomeView, name='Página de inicio'),
     path('admin/', admin.site.urls),
 ]
