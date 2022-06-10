6e82726e4ee30f8864ccedb052a7c30f526e4ce7
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-09 23:55:30 -0500

    Clase Mascota para servidor

--- src/mascotas/models.py
+++ src/mascotas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField(max_length=300)
+    dueño = models.CharField(max_length=50)
+    tiempo = models.IntegerField()
