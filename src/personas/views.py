c8c9e997b48f180d3e7599f7bac1ad4c80801493
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-14 12:35:03 -0500

    Django 03 / enviando objeto al contexto

--- src/personas/views.py
+++ src/personas/views.py
@@ -5,7 +5,6 @@ from .models import Persona
 def personaTestView(request):
     obj = Persona.objects.get(id = 1)
     context = {
-        'nombre' : obj.nombre,
-        'edad' : obj.edad,
+        'objeto' : obj,
     }
     return render(request, 'personas/test.html', context)
