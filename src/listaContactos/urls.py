731030c3ee6d49795cf0cc562b7fada56323e80f
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-15 18:57:52 -0500

    Django 03 / Search completo

--- src/listaContactos/urls.py
+++ src/listaContactos/urls.py
@@ -18,13 +18,14 @@ from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
 #from inicio.views import presentacionView
-from personas.views import personaTestView, personaCreateView
+from personas.views import personaTestView, personaCreateView, searchForHelp
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', myHomeView, name='home'),
+    path('', myHomeView, name='PaginaInicio'),
     #path('saludos/', presentacionView),
-    path('another/', anotherView, name = 'otro'),
-    path('persona/', personaTestView, name = 'testViewPersona'),
-    path('agregar/', personaCreateView, name = 'createPersona'),
+    path('people', personaTestView, name = 'personas'),
+    path('add', personaCreateView, name = 'AgregarPersonas'),
+    path('search', searchForHelp, name = 'buscar'),
+    path('another', anotherView, name = 'otra'),
+    path('admin/', admin.site.urls),
 ]
