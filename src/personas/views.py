755902a5907e3b0dc3611fae3895bb0b78058e3e
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-06-16 00:48:12 -0500

    Django 03 / identificando cuándo es post o get

--- src/personas/views.py
+++ src/personas/views.py
@@ -11,8 +11,10 @@ def personaTestView(request):
     return render(request, 'personas/descripcion.html', context)
 
 def personaCreateView(request):
-    print('GET: ', request.GET)
-    print('POST: ', request.POST)
+    print(request)
+    if request.method == 'POST':
+        nombre = request.POST.get('q')
+        print(nombre)
     context = {}
     return render(request, 'personas/personasCreate.html', context)
 
